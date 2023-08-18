eb5de2dcecb855f5e68b166ade1b59dc271bc452
Federico Stempler <zackmango@gmail.com>
2023-08-18 14:52:24 -0300

    CSS Fix

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Schoolex</Text>      
+      <Text style={styles.text}>SCHOOLEX</Text>     
+      <Text style={styles.emoji}>📐📚✏️</Text>
+      
+      
+      <View style={styles.logInContainer}>
+        <TextInput
+      style={styles.input}
+      placeholder='User'
+      ></TextInput>
+      <TextInput
+      style={styles.input}
+      placeholder='Password'
+      ></TextInput>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.btnText}>Log In</Text>
+      </TouchableOpacity>      
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+      <Text style={styles.copyright}>Copyright © 2023 Stempler®. All rights reserved.</Text>
+    </View> 
   );
 }
 
@@ -15,10 +33,44 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#171717',    
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',  
+    padding: 25,  
   },
   text: {
     fontSize: 50,
-    color: '#ddd457'
+    color: '#FB6076',
+    margin: 50,
   },
+  emoji: {
+    fontSize: 75,
+  },
+  input: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'gray',
+    backgroundColor: '#d9d9d9',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  logInContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    width: '75%',
+    marginBottom: 55,
+  },
+  button: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'gray',
+    backgroundColor: '#d9d9d9',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,    
+    alignItems: 'center',
+  },
+  copyright: {
+    color: 'black',
+  }
 });
